5ae343347ecfefb22c3909c09dce27e950f337c7
naholonaholo <sora13041304@gmail.com>
2026-08-07 23:49:31 +0900

    Update 2REF12EBN___2.ts

--- cloud/devices/2REF12EBN___2.ts
+++ cloud/devices/2REF12EBN___2.ts
@@ -4,22 +4,36 @@ import { type Connection } from '../homeassistant'
 import { Metadata } from '../thinq'
 
 export default class Device extends HADevice {
-    private pollTimer?: NodeJS.Timeout
-
-    constructor(HA: Connection, readonly thinq: Thinq1Device, meta: Metadata) {
+    constructor(
+        HA: Connection,
+        readonly thinq: Thinq1Device,
+        meta: Metadata,
+    ) {
         super(HA, thinq.id)
-        this.setConfig(HADevice.config(meta, { name: 'LG Refrigerator (캡처중)' }))
+
+        // HA 엔티티는 필요없으니 최소한만 (availability 정도)
+        this.setConfig(HADevice.config(meta, { name: 'LG Fridge (debug capture)' }))
+
         thinq.on('data', (buf) => {
-            console.log('RAW:', buf.length, 'bytes ->', buf.toString('hex'))
+            console.log(
+                `[FRIDGE-CAPTURE ${thinq.id}] len=${buf.length} hex=${buf.toString('hex')}`,
+            )
         })
     }
 
+    monTimer: ReturnType<typeof setInterval> | undefined
+
     start() {
+        // WTDN3와 동일한 방식으로 폴링을 강제 시작
         this.thinq.send({ Cmd: 'Mon', CmdOpt: 'Start' })
 
-        this.pollTimer = setInterval(() => {
-            console.log('[poll] requesting data...')
-            this.thinq.send({ Cmd: 'Mon', CmdOpt: 'Data' })
-        }, 3000)
+        // 세션이 만료될 가능성에 대비해 주기적으로 재전송
+        this.monTimer = setInterval(() => {
+            this.thinq.send({ Cmd: 'Mon', CmdOpt: 'Start' })
+        }, 45_000)
+    }
+
+    setProperty(prop: string, mqttValue: string) {
+        // 진단 단계라 제어는 구현하지 않음
     }
 }
