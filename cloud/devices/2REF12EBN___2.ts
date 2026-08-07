87edbfb2272423b1c2b32a1f2d5f5e3ebeb26c73
naholonaholo <sora13041304@gmail.com>
2026-08-07 22:57:19 +0900

    Update 2REF12EBN___2.ts

--- cloud/devices/2REF12EBN___2.ts
+++ cloud/devices/2REF12EBN___2.ts
@@ -4,14 +4,22 @@ import { type Connection } from '../homeassistant'
 import { Metadata } from '../thinq'
 
 export default class Device extends HADevice {
+    private pollTimer?: NodeJS.Timeout
+
     constructor(HA: Connection, readonly thinq: Thinq1Device, meta: Metadata) {
         super(HA, thinq.id)
-        this.setConfig(HADevice.config(meta, { name: 'LG Refrigerator  (캡처중)' }))
+        this.setConfig(HADevice.config(meta, { name: 'LG Refrigerator (캡처중)' }))
         thinq.on('data', (buf) => {
             console.log('RAW:', buf.length, 'bytes ->', buf.toString('hex'))
         })
     }
+
     start() {
         this.thinq.send({ Cmd: 'Mon', CmdOpt: 'Start' })
+
+        this.pollTimer = setInterval(() => {
+            console.log('[poll] requesting data...')
+            this.thinq.send({ Cmd: 'Mon', CmdOpt: 'Data' })
+        }, 3000)
     }
 }
