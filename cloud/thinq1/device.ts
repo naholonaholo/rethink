85fa2dc8c7d2cea91254cb1d8325d0aba359b044
naholonaholo <sora13041304@gmail.com>
2026-08-26 10:56:08 +0900

    Update device.ts

--- cloud/thinq1/device.ts
+++ cloud/thinq1/device.ts
@@ -4,7 +4,6 @@ import { Connection } from './connection'
 import { getDeviceMetadata } from './http'
 import { Metadata } from '../thinq'
 import { randomUUID } from 'node:crypto'
-import { registerDevice, unregisterDevice } from './status-bridge'
 
 type ConWithExtra = Connection & {
     deviceObj?: Device
@@ -28,12 +27,6 @@ export class Device extends TypedEmitter<DeviceEvents> {
     ) {
         super()
         con.deviceObj = this
-        // HTTPS diagmon 쪽(http.ts → status-bridge.ts)에서 이 기기를 찾아
-        // 상태를 주입할 수 있도록 등록. device.ts는 http.ts를 그대로
-        // import하지만(getDeviceMetadata), http.ts는 device.ts를 직접
-        // import하지 않고 status-bridge.ts를 통해서만 접근하므로 순환
-        // 참조가 생기지 않는다.
-        registerDevice(id, this)
         con.on('status', (packet) => {
             this.lastReport = packet
             this.emit('data', packet)
@@ -44,7 +37,6 @@ export class Device extends TypedEmitter<DeviceEvents> {
                 this.emit('close')
                 con.deviceObj = undefined
             }
-            unregisterDevice(id, this)
         })
     }
 
